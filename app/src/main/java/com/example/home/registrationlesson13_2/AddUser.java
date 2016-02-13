5534fe45901b6adcc6fb9b748ef9611fa076404b
ole4ka <ole4ka@users.noreply.github.com>
2016-02-14 00:50:55 +0200

    test 2

--- app/src/main/java/com/example/home/registrationlesson13_2/AddUser.java
+++ app/src/main/java/com/example/home/registrationlesson13_2/AddUser.java
@@ -10,7 +10,7 @@ import android.widget.EditText;
 public class AddUser extends AppCompatActivity
 {
     User user = new User();
-/// coment
+/// comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
