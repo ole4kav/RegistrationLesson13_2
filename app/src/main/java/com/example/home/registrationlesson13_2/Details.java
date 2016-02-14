735a7ad26066b4b992d9c5ab101df02f23519328
ole4ka <ole4ka@users.noreply.github.com>
2016-02-15 00:31:17 +0200

    change for Details activity

--- app/src/main/java/com/example/home/registrationlesson13_2/Details.java
+++ app/src/main/java/com/example/home/registrationlesson13_2/Details.java
@@ -1,7 +1,9 @@
 package com.example.home.registrationlesson13_2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Details extends AppCompatActivity
 {
@@ -10,5 +12,18 @@ public class Details extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        Intent startingIntent = getIntent();
+
+        if (startingIntent.hasExtra("User")){
+            User newUser = startingIntent.getParcelableExtra("User");
+
+            EditText editText = (EditText) findViewById(R.id.nameEditTextEdit);
+            editText.setText(newUser.name);
+
+            //TextView textView = (TextView) findViewById(R.id.textView);
+            //textView.setText("The towns of "+name+":");
+        }
+
     }
 }
--- app/src/main/java/com/example/home/registrationlesson13_2/MainActivity.java
+++ app/src/main/java/com/example/home/registrationlesson13_2/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -14,8 +15,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener
 {
 
-    ArrayList<User> userslist = new ArrayList<User>();
-
+    ArrayList<User> userslist = new ArrayList<>();
 
     static final int ADD_USER = 1;
     static final int DETAILS = 2;
@@ -25,25 +25,44 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         buildListView();
 
     }
 
 
     public void buildListView(){
-        ArrayAdapter<User> adapter = new ArrayAdapter<User>(this, android.R.layout.simple_list_item_1,userslist);
+        ArrayAdapter<User> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,userslist);
 
-        ListView mListView = (ListView) findViewById(R.id.listView);
+        final ListView mListView = (ListView) findViewById(R.id.listView);
         mListView.setAdapter(adapter);
 
-        mListView.setOnItemClickListener(this);
+        mListView.setOnItemLongClickListener(new OnItemLongClickListener()
+        {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                User user = userslist.get(position);
+
+                Intent intentEdit = new Intent(MainActivity.this,Details.class);
+                intentEdit.putExtra("User", user);
+                startActivity(intentEdit);
+
+
+                //Toast.makeText(getApplicationContext(),mListView.getItemAtPosition(position).toString(), Toast.LENGTH_LONG).show();
+
+
+                return true;
+            }
+        });
     }
+
+
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
     }
+
     public void clickAddUserBtn(View v) {
         Intent intentAddUser = new Intent(this,AddUser.class );
         startActivityForResult(intentAddUser, ADD_USER);
@@ -54,12 +73,14 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         if (requestCode == ADD_USER){
             if (resultCode == RESULT_OK){
                 User newUser = data.getParcelableExtra("data");
-                if ((userslist==null)||(userslist.size()==0)){
+                if (userslist==null){
+                    userslist = new ArrayList<>();
                     userslist.add(newUser);
                 }
-                else {
+                else
+                {
                     for (int i = 0; i<userslist.size(); i++){
-                        if(equals(newUser)) {
+                        if(userslist.get(i).equals(newUser)) {
                             Toast.makeText(this, "The user "+newUser.name+" "+newUser.last+" is already exist",Toast.LENGTH_LONG).show();
                             return;
                         }
